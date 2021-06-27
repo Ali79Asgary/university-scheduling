d29070c98428a44f367317efdff9e38b21c87a32
Nima <pn2120551@gmail.com>
2021-06-28 03:43:10 +0430

    Removing index.js

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ config.MONGODB_PORT = process.env.MONGODB_PORT || config.MONGODB_PORT
 /*
     Connect to database using the credentials defined in config/{development/production}-config.json file
 */
-mongoose.connect(`mongodb://${config.MONGODB_HOST}:${config.MONGODB_PORT}/${config.MONGODB_DB_NAME}`, 
-    config.MONGOOSE_config)
+const DB_URL = `mongodb://${config.MONGODB_HOST}:${config.MONGODB_PORT}/${config.MONGODB_DB_NAME}`
+mongoose.connect(DB_URL, config.MONGOOSE_config)
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.error('Could not connect to MongoDB...'));
 
