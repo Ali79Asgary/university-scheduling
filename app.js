be242125367f7c0b21d457be15dd17cd9ef239fc
Nima <pn2120551@gmail.com>
2021-07-08 02:26:42 +0430

    TimeTableBells Api Complete

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const users = require('./routes/users');
 const auth = require('./routes/auth');
 const courses = require('./routes/course')
+const timeTableBells = require('./routes/timeTableBells')
 const app = express();
 
 /*
@@ -15,5 +16,6 @@ app.use(express.json());
 app.use('/api/Users', users);
 app.use('/api/Auth', auth);
 app.use('/api/Courses', courses);
+app.use('/api/TimeTableBells', timeTableBells)
 
 module.exports = app
--- middleware/isAdminOrMaster.js
+++ middleware/isAdminOrMaster.js
@@ -1,6 +1,6 @@
 module.exports = function (req, res, next) {
 
-    if (req.user.rule !== "Master" || req.user.rule !== "Admin")
+    if (req.user.rule !== "Master" && req.user.rule !== "Admin")
         return res.status(403).send('Only masters and admins can access to this API.');
 
     next();
