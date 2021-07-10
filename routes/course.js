4c37945606eb8f7ad638d491992266c7882a78da
Mahdi <mahdiw.ghiasi@gmail.com>
2021-07-11 00:45:38 +0430

    Courses API completed.

--- routes/course.js
+++ routes/course.js
@@ -1,74 +1,61 @@
 const { Course } = require("../models/course");
+const { User } = require("../models/user");
+const { Master } = require("../models/master");
+const { Bell } = require("../models/bell");
+const { Day } = require("../models/day");
+const { TimeTableBell } = require("../models/timeTableBell");
+const { TimeTable } = require("../models/timeTable");
 const isAuthenticated = require("../middleware/isAuthenticated");
 const isAdmin = require("../middleware/isAdmin");
 const isMaster = require("../middleware/isMaster");
 const express = require("express");
-const router = express.Router();
 const mongo = require("mongodb");
+const router = express.Router();
 
 /*
     /api/Courses
 */
-
-function splitArrayIntoChunksOfLen(arr, len) {
-    var chunks = [],
-        i = 0,
-        n = arr.length;
-    while (i < n) {
-        chunks.push(arr.slice(i, (i += len)));
-    }
-    return chunks;
-}
-
 router.get("/", isAuthenticated, isAdmin, async (req, res) => {
     const criteria = {
         $and: [
-            { title: { $regex: `/${req.body.search}/`, $options: "i" } },
-            { unitCounts: req.body.unitCount },
+            { title: { $regex: `${req.query.search}`, $options: "i" } },
+            { unitCounts: req.query.unitCount },
         ],
     };
-    await Course.find(criteria, function (err, doc) {
-        if (err) {
-            return res.status(404).json({
-                success: false,
-                message: "The requested course does not exist!",
-            });
-        }
+    const courses = await Course.find(criteria);
 
-        if (doc.length == 0) {
-            return res.status(200).json({
-                success: true,
-                message: "The requested course found successfully!",
-                data: {
-                    list: [],
-                    count: 0,
-                    page: 0,
-                    totalPages: 0,
-                },
-            });
-        }
-
-        resList = splitArrayIntoChunksOfLen(doc, req.body.PageSize);
-        return res.status(200).json({
-            success: true,
-            message: "The requested courses found successfully!",
-            data: {
-                list:
-                    resList.length < req.body.Page
-                        ? resList[resList.length - 1]
-                        : resList[req.body.Page],
-                count:
-                    resList.length < req.body.Page
-                        ? resList[resList.length - 1].length
-                        : resList[req.body.Page].length,
-                page:
-                    resList.length < req.body.Page
-                        ? resList.length
-                        : req.body.page,
-                totalPages: resList.length,
-            },
+    if (courses.length === 0)
+        return res.status(404).json({
+            success: false,
+            message: "The requested course does not exist!",
         });
+
+    const courseList = [];
+    for (const course of courses) {
+        courseList.push(
+            {
+                title: course.title,
+                id: course._id,
+                unitsCount: course.unitCounts
+            }
+        );
+    }
+
+    const totalPagesCount = Math.ceil(courseList.length / req.query.PageSize);
+    const startIndex = (req.query.Page - 1) * req.query.PageSize;
+    const trimmedList = courseList.slice(startIndex, startIndex + parseInt(req.query.PageSize));
+
+    return res.status(200).json({
+        success: true,
+        message: "The requested courses found successfully!",
+        data: {
+            list: trimmedList,
+            count: trimmedList.length,
+            page: req.query.Page,
+            totalPages: totalPagesCount
+        },
     });
+
 });
 
 //done
@@ -161,95 +148,143 @@ router.delete("/:id", isAuthenticated, isAdmin, async (req, res) => {
 
 //done*
 router.get("/:id/TimeTables", isAuthenticated, async (req, res) => {
-    await Course.findById(req.params["id"], function (err, doc) {
-        if (err) {
-            return res.status(404).json({
-                success: false,
-                message: "The requested course does not exist!",
-            });
-        }
+    const course = await Course.findById(req.params["id"]);
+    if (!course) {
+        return res.status(404).json({
+            success: false,
+            message: "The requested course does not exist!",
+        });
+    }
 
-        timeTable = doc.timeTables;
-        if (timeTable.length == 0) {
-            return res.status(200).json({
-                success: true,
-                message: "The requested course found successfully!",
-                data: {
-                    list: [],
-                    count: 0,
-                    page: 0,
-                    totalPages: 0,
-                },
-            });
-        }
-        resList = splitArrayIntoChunksOfLen(timeTable, req.body.PageSize);
-
+    if (course.timeTables.length === 0) {
         return res.status(200).json({
             success: true,
-            message: "The requested course found successfully!",
+            message: "The requested course has not any Time Tables!",
             data: {
-                list:
-                    resList.length < req.body.Page
-                        ? resList[resList.length - 1]
-                        : resList[req.body.Page],
-                count:
-                    resList.length < req.body.Page
-                        ? resList[resList.length - 1].length
-                        : resList[req.body.Page].length,
-                page:
-                    resList.length < req.body.Page
-                        ? resList.length
-                        : req.body.page,
-                totalPages: resList.length,
+                list: [],
+                count: 0,
+                page: 0,
+                totalPages: 0,
             },
         });
+    }
+
+    const resList = [];
+    const timeTableBells = [];
+
+    for (const timeTableId of course.timeTables) {
+
+        const timeTable = await TimeTable.findOne({_id: timeTableId});
+        const master = await Master.findOne({_id: timeTable.master});
+        const user = await User.findOne({_id: master.user});
+
+        for (const timeTableBellId of timeTable.timeTableBells) {
+
+            const timeTableBell = await TimeTableBell.findOne({_id: timeTableBellId});
+            const bell = await Bell.findOne({_id: timeTableBell.Bell});
+            const day = await Day.findOne({_id: timeTableBell.Day});
+
+            timeTableBells.push(
+                {
+                    id: timeTableBell._id,
+                    bell: {
+                        id: bell._id,
+                        label: bell.label,
+                        bellOfDay: bell.bellOfDay
+                    },
+                    day: {
+                        id: day._id,
+                        label: day.label,
+                        dayOfWeek: day.dayOfWeek
+                    }
+                });
+        }
+        resList.push({
+            id: timeTable._id,
+            master: {
+                id: master._id,
+                userId: user._id,
+                user: {
+                    id: user._id,
+                    firstName: user.firstName,
+                    lastName: user.lastName,
+                    code: user.code,
+                    role: user.role
+                }
+            },
+            timeTableBells: timeTableBells
+        });
+    }
+    const totalPagesCount = Math.ceil(resList.length / req.query.PageSize);
+    const startIndex = (req.query.Page - 1) * req.query.PageSize;
+    const trimmedList = resList.slice(startIndex, startIndex + parseInt(req.query.PageSize));
+
+    return res.status(200).json({
+        success: true,
+        message: "The requested course found successfully!",
+        data: {
+            list: trimmedList,
+            count: trimmedList.length,
+            page: parseInt(req.query.Page),
+            totalPages: totalPagesCount
+        },
     });
 });
 
 //done*
 router.get("/:id/Masters", isAuthenticated, async (req, res) => {
-    await Course.findById(req.params["id"], function (err, doc) {
-        if (err) {
-            return res.status(404).json({
-                success: false,
-                message: "The requested course does not exist!",
-            });
-        }
+    const course = await Course.findById(req.params["id"]);
+    if (!course) {
+        return res.status(404).json({
+            success: false,
+            message: "The requested course does not exist!",
+        });
+    }
 
-        masters = doc.masters;
-        if (masters.length == 0) {
-            return res.status(200).json({
-                success: true,
-                message: "The requested course found successfully!",
-                data: {
-                    list: [],
-                    count: 0,
-                    page: 0,
-                    totalPages: 0,
-                },
-            });
-        }
-        resList = splitArrayIntoChunksOfLen(masters, req.body.PageSize);
-
+    if (course.masters.length === 0) {
         return res.status(200).json({
             success: true,
             message: "The requested course found successfully!",
             data: {
-                list:
-                    resList.length < req.body.Page
-                        ? resList[resList.length - 1]
-                        : resList[req.body.Page],
-                count:
-                    resList.length < req.body.Page
-                        ? resList[resList.length - 1].length
-                        : resList[req.body.Page].length,
-                page:
-                    resList.length < req.body.Page
-                        ? resList.length
-                        : req.body.page,
-                totalPages: resList.length,
+                list: [],
+                count: 0,
+                page: 0,
+                totalPages: 0,
             },
         });
+    }
+    const resList = [];
+    for (const masterId of course.masters) {
+
+        const master = await Master.findOne({user: masterId});
+        const user = await User.findOne({_id: master.user});
+
+        resList.push({
+            id: master._id,
+            userId: user._id,
+            user: {
+                id: user._id,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                code: user.code,
+                role: user.role
+            }
+        });
+    }
+
+    const totalPagesCount = Math.ceil(resList.length / req.query.PageSize);
+    const startIndex = (req.query.Page - 1) * req.query.PageSize;
+    const trimmedList = resList.slice(startIndex, startIndex + parseInt(req.query.PageSize));
+
+    return res.status(200).json({
+        success: true,
+        message: "The requested course found successfully!",
+        data: {
+            list: trimmedList,
+            count: trimmedList.length,
+            page: parseInt(req.query.Page),
+            totalPages: totalPagesCount
+        },
     });
 });
 
